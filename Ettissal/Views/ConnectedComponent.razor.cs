63c7ef623f70a24e8847c97d91d66cf4a7218070
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-12-17 00:26:50 +0100

    COMPONENTS: Initialize Connected Component

---
+++ Ettissal/Views/ConnectedComponent.razor.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Ettissal.Views
+{
+    public partial class ConnectedComponent : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment Online { get; set; }
+
+        [Parameter]
+        public RenderFragment Offline { get; set; }
+
+        public bool IsOnline { get; set; }
+
+        [JSInvokable("Connection.StatusChanged")]
+        public void OnConnectionStatusChanged(bool isOnline)
+        {
+            if (IsOnline != isOnline)
+            {
+                IsOnline = isOnline;
+            }
+
+            StateHasChanged();
+        }
+    }
+}
