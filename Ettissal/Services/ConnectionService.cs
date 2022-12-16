2ee42e86258bfeff26ae8c6682a467a7f14b8cd4
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-12-17 00:21:35 +0100

    FOUNDATIONS: Initialize Connection Service

---
+++ Ettissal/Services/ConnectionService.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.JSInterop;
+
+namespace Ettissal.Services
+{
+    public class ConnectionService : IConnectionService
+    {
+        private readonly IJSRuntime jsRuntime;
+
+        public ConnectionService(IJSRuntime jsRuntime) =>
+            this.jsRuntime = jsRuntime;
+
+
+        public ValueTask<bool> CheckConnectivityAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ettissal/Services/IConnectionService.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace Ettissal.Services
+{
+    public interface IConnectionService
+    {
+        ValueTask<bool> CheckConnectivityAsync();
+    }
+}
