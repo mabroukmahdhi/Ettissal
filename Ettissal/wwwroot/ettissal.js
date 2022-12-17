c8b20fb89cfd59834aa12d02c77e8f4183d61304
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-12-17 01:17:01 +0100

    COMPONENTS: Fixing JS Code

--- Ettissal/wwwroot/ettissal.js
+++ Ettissal/wwwroot/ettissal.js
@@ -1,23 +1,20 @@
-let handler;
+let connectedComponentEventHandler;
 
-window.Connection = {
-    Initialize: function (interop) {
-
-        handler = function () {
-            interop.invokeMethodAsync("Connection.StatusChanged", navigator.onLine);
-        }
-
-        window.addEventListener("online", handler);
-        window.addEventListener("offline", handler);
-
-        handler(navigator.onLine);
-    },
-    Dispose: function () {
-
-        if (handler != null) {
-
-            window.removeEventListener("online", handler);
-            window.removeEventListener("offline", handler);
-        }
+export function initializeConnectedComponent(interop) {
+    connectedComponentEventHandler = function () {
+        interop.invokeMethodAsync("Connection.StatusChanged", navigator.onLine);
     }
-};
+
+    window.addEventListener("online", handler);
+    window.addEventListener("offline", handler);
+
+    connectedComponentEventHandler(navigator.onLine);
+}
+
+export function disposeConnectedComponent() {
+    if (connectedComponentEventHandler != null) {
+
+        window.removeEventListener("online", handler);
+        window.removeEventListener("offline", handler);
+    }
+}
